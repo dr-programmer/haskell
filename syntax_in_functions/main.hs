e9dd4e9e40f4d7c0e427cf7558d231efbb90cf8e
dr-programmer <drtopuzov@gmail.com>
2025-01-04 13:57:39 +0200

    Finished syntax in functions

--- syntax_in_functions/main.hs
+++ syntax_in_functions/main.hs
@@ -31,3 +31,58 @@ length' (_:xs) = 1 + length' xs
 capital :: String -> String
 capital "" = "Empty string, whoops!"
 capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"
+    | otherwise     = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          (skinny, normal, fat) = (18.5, 25.0, 30.0)
+
+max' :: (Ord a) => a -> a -> a
+max' a b 
+    | a > b     = a
+    | otherwise = b
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a > b     = GT
+    | a == b    = EQ
+    | otherwise = LT
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [ bmi w h | (w, h) <- xs ]
+    where bmi weight height = weight / height ^ 2
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h = 
+    let sideArea = 2 * pi * r * h
+        topArea  = pi * r ^ 2
+    in sideArea + 2 * topArea
+
+calcBmisFat :: (RealFloat a) => [(a, a)] -> [a]
+calcBmisFat xs = [ bmi | (w, h) <- xs, let bmi = w / h ^ 2; fat = 25.0, bmi >= fat ]
+
+head'' :: [a] -> a
+head'' xs = case xs of [] -> error "No head for empty lists!"
+                       (x:_) -> x
+
+describeList :: [a] -> String
+describeList xs = "The list is " ++ case xs of [] -> "empty."
+                                               [x] -> "a singleton list."
+                                               xs -> "a longer list."
+
+describeList' :: [a] -> String
+describeList' xs =
+    let what [] = "empty."
+        what [x] = "a singleton list."
+        what xs = "a longer list."
+    in "The list is " ++ what xs
+
+describeList'' :: [a] -> String
+describeList'' xs = "The list is " ++ what xs
+    where what [] = "empty."
+          what [x] = "a singleton list."
+          what xs = "a longer list."
